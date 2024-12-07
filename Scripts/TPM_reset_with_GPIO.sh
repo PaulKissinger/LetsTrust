55093961cb263cf102045b24ab345241c01294e1
Paul Kissinger <paul.kissinger@letstrust.de>
2024-12-07 22:39:22 +0100

    Update TPM_reset_with_GPIO.sh
    exchange raspi-gpio with pinctrl

--- Scripts/TPM_reset_with_GPIO.sh
+++ Scripts/TPM_reset_with_GPIO.sh
@@ -3,13 +3,13 @@
 # Reset the TPM without reboot
 # only use for development!
 
-raspi-gpio set 24 op
-raspi-gpio set 24 dh
+pinctrl set 24 op
+pinctrl set 24 dh
 tpm2_getrandom 10
-raspi-gpio set 24 dl
+pinctrl set 24 dl
 tpm2_getrandom 10
 sleep 0.1
-raspi-gpio set 24 dh
+pinctrl set 24 dh
 tpm2_getrandom 10
 tpm2_startup -c
 tpm2_getrandom 10
